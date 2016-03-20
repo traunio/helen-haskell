fda3637c10aff4369938207878285ff6b2a0f64a
Tapani Raunio <traunio@users.noreply.github.com>
2016-03-21 00:02:31 +0200

    Implement main function and ensure nice output

--- helen_main.hs
+++ helen_main.hs
@@ -2,6 +2,7 @@
 
 import Helen_skaba
 import System.IO
+import Data.List
 
 -- horrible hack function, to read date. Will crash with wrong input :)
 readDate :: String -> Date
@@ -19,32 +20,79 @@ readit xs = PD (TS date time) price
         time = line !! 1
         price = read (line !! 2 ) :: Price
 
--- -- Helper function to
--- lastHour :: PriceData TimeStamp Price -> Bool
--- lastHour (PD (TS _ time) _)
---   | time == "23-00" = True
---   | otherwise       = False
+lastHour :: PriceData TimeStamp Price -> Bool
+lastHour (PD (TS _ time) _)
+  | time == "23-00" = True
+  | otherwise       = False
+
+type Unit = PriceData TimeStamp Price
+
+lastReq :: Reqs -> [Unit] -> Reqs
+lastReq req [] = req
+lastReq (Full loss _) (_:xs) = lastReq (Empty loss) xs
+lastReq (Empty loss) (x:xs) = lastReq (Full loss $ value x) xs
+
+giveNextD :: TimeStamp -> TimeStamp
+giveNextD (TS (D d m y) _)
+  | d == 28 && y `elem` [1996,2000..2096] = TS (D (d+1) m y) "14-15"
+  | d == 28 && m == 2 = TS (D 1 3 y) "14-15"
+  | d == 31 && m == 12 = TS (D 1 1 (y+1)) "14-15"
+  | d == 31 && m `elem` [1,3,5,7,8,10] = TS (D 1 (m+1) y) "14-15"
+  | d == 30 && m `elem` [2,4,6,9,11] = TS (D 1 (m+1) y) "14-15"
+  | otherwise = TS (D (d+1) m y) "14-15"
+
+
+cutTime :: TimeStamp -> [Unit] -> [Unit]
+cutTime _ [] = []
+cutTime ab@(TS (D d m y) hours) xs
+  | hours == "14-15" || hours > "14-15"= takeWhile (nextDay . tag) xs
+  | otherwise = takeWhile (today . tag) xs
+  where nextDay time= time < giveNextD ab
+        today time = time < TS (D d m y) "14-15"
+
+accumulate :: (Reqs, [Unit],[Unit]) -> Unit -> (Reqs, [Unit], [Unit])
+accumulate (a,b,c) d =
+  if lastHour d then flush else (a,b,day)
+  where day = c++[d]
+        highs = localMax day
+        lows = localMin day
+        optimal = snd (bestMoney a highs lows)
+        cut = cutTime (tag $ head c) optimal
+        eqs (TS _ hours) = hours == "14-15"
+        newA = lastReq a cut
+        newB = b ++ cut
+        newC = dropWhile (not . eqs . tag ) $ tail day
+        flush = (newA, newB, newC)
+
+
+nicePrint :: (Bool, String) -> Unit -> (Bool, String)
+nicePrint (full, xs) (PD ts p)
+  | full = (False, xs++"\nSell order " ++ niceTime ts ++ " Price: " ++ show p)
+  | otherwise = (True, xs++"\nBuy order " ++ niceTime ts ++ " Price: "++ show p)
+  where niceTime (TS (D d m y) h) = intercalate "-" [show d,show m,show y] ++ " " ++ h ++ "."
 
 
 main :: IO()
 main = do
-  putStrLn "Greetings, yeehaa"
+  putStrLn "Running optimisation"
   inph <- openFile "elspot_test.txt" ReadMode
-  inpStr <- hGetLine inph
-  putStrLn $ "First line: " ++ inpStr
+  _ <- hGetLine inph
   sndStr <- hGetContents inph
   let allLines = map readit $ lines sndStr
-      highs = localMax allLines
-      lows = localMin allLines
-      result = bestMoney (Full 0.2 0) highs lows
+      (a,b,c) = foldl accumulate (Full 0.2 0,[],[]) allLines
+      highs = localMax c
+      lows = localMin c
+      resultList = (++) b $ snd (bestMoney a highs lows)
+      profit = calcPrice (Full 0.2 0) resultList
+      cycles = length resultList `div` 2 +1
+      totalres = snd $ foldl nicePrint (True,"") resultList
 
 
-  putStrLn $ "Best result " ++ show (fst result) ++ " e."
-  putStrLn $ unlines $ map show (snd result)
-  -- putStrLn "All the highs:"
-  -- putStrLn $ unlines $ map show highs
-  --
-  -- putStrLn "All the lows:"
-  -- putStrLn $ unlines $ map show lows
+  outh <- openFile "results.txt" WriteMode
+  hPutStrLn outh $ "Profit made " ++ show profit
+  hPutStrLn outh $ "Total cycles " ++ show cycles
+  hPutStrLn outh totalres --(unlines $ map show resultList)
+
 
   hClose inph
+  hClose outh
